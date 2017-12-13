2b33dbfa0cc1a83662620b237ba2d5e33cd8403d
unknown <nildo.777@gmail.com>
2017-12-13 14:02:57 -0200

    pequenas modificações

--- src/pages/adiciona-endereco/adiciona-endereco.ts
+++ src/pages/adiciona-endereco/adiciona-endereco.ts
@@ -1,3 +1,4 @@
+import { Http } from '@angular/http';
 import { UtilProvider } from './../../providers/util/util';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
@@ -11,11 +12,14 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class AdicionaEnderecoPage {
   formCEP: FormGroup;
   endereco: any;
+  achouEndereco:boolean = false;
+
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams, 
     public alertCtrl:AlertController,
-    public util: UtilProvider
+    public util: UtilProvider,
+    public http: Http
   ) {
     this.formCEP = new FormGroup({
       'cep': new FormControl('', [Validators.required, Validators.pattern('[0-9]{5}[0-9]{3}')])
@@ -27,9 +31,15 @@ export class AdicionaEnderecoPage {
   }
 
   submitCEP(){
+
+    
     this.util.buscarPeloCEP(this.formCEP.value.cep)
       .subscribe(endereco => {
+
+        console.log(endereco);
+
         this.endereco = endereco.json();
+
         if(endereco.json().erro){
           let alert = this.alertCtrl.create({
             title: 'adiciona-endereco',
@@ -50,8 +60,11 @@ export class AdicionaEnderecoPage {
           });
           alert.present();
         }
+        else{
+          this.achouEndereco = true;
+        }
+        console.log(this.endereco);
       })
-    console.log(this.formCEP);
   }
   naoSabeCEP(){
     // Import the AlertController from ionic package 
